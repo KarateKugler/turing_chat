8e83214e3a9005033c355d4f0c5230a563e4508f
Max <milian.kugler@gmail.com>
2025-05-21 20:56:41 +0200

    prep for v0.0.1 upload
    * made and added icon
    * signed the app bundle with keytool
    * changed manifest, package name/namespace and version names

--- lib/widgets/app_logo.dart
+++ lib/widgets/app_logo.dart
@@ -8,24 +8,27 @@ class AppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        gradient: LinearGradient(
-          colors: [
-            Theme.of(context).colorScheme.tertiaryContainer,
-            Theme.of(context).colorScheme.secondaryContainer,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.onSurfaceVariant,
+              Theme.of(context).colorScheme.onSurface,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          border: Border.all(color: Theme.of(context).colorScheme.primary),
         ),
-        border: Border.all(color: Theme.of(context).colorScheme.primary),
-      ),
-      child: Icon(
-        Icons.chat,
-        size: 60,
-        color: Theme.of(context).colorScheme.primary,
-      ),
+        child: SizedBox(
+          height: 80,
+          width: 80,
+          child: Image.asset(
+            'assets/icon/icon.png',
+            fit: BoxFit.contain,
+          ),
+        ),
     );
   }
 }
